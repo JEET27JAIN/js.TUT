1c413420b08b2c6f2493d0a6f517e33dffef0dd2
JEET27JAIN <168724624+JEET27JAIN@users.noreply.github.com>
2025-03-31 11:39:00 +0000

    conversion of datatypes

---
+++ 01_basic/03_conversionoperation.js
@@ -0,0 +1,24 @@
+let score = null // let score = "33" //let score = "33abc" //let score = undefined
+
+//console.log(typeof score)  // const {score} = req.body
+//console.log(typeof (score))  
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber)
+//console.log( valueInNumber)
+
+// "33" => 33
+//"33abc" => Nan
+// true or false => 1/0 
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
+
+
