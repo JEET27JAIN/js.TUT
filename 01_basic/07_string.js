f4e3fc14c82aff73dd5280fb4c0c74111e6bfbf6
JEET27JAIN <168724624+JEET27JAIN@users.noreply.github.com>
2025-04-02 13:50:38 +0000

    string types

---
+++ 01_basic/07_string.js
@@ -0,0 +1,34 @@
+const name = "Jeet"
+const repoCount = 50 
+
+//console.log(name + repoCount + " Value");
+
+c//onsole.log(`hello my name is ${name} and my repo count ${repoCount}`);
+
+const gameName = new String('jeath-jt')
+//console.log(gameName[0]);
+
+//console.log(gameName.__proto__);
+
+//console.log(gameName.length);
+//console.log(gameName.toUpperCase());
+//console.log(gameName.charAt(2));
+//console.log(gameName.indexOf('e'));
+
+const newString = gameName. substring(0,4)
+//console.log(newString);
+
+const anotherString = gameName.slice(-8,4)
+//console.log(anotherString);
+
+const newStringOne = "   jeet     "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+//const url = ++++++
+//console.log(url.replace('%20', '-'))
+//console.log(url.includes(''sunder))
+ 
+//also read about different types of string
+
+
