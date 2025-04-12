6b270efe35f4528ad8f089a8fad11248399c4924
JEET27JAIN <168724624+JEET27JAIN@users.noreply.github.com>
2025-04-12 10:30:16 +0000

    taking parameter in function

--- 03_basic/01_function.js
+++ 03_basic/01_function.js
@@ -37,7 +37,7 @@ function loginUserMessage(username = "sam"){
 
 // console.log(loginUserMessage("hitesh"))
 // console.log(loginUserMessage("hitesh"))
-
+//`` for sting interpolation
 
 function calculateCartPrice(val1, val2, ...num1){
     return num1
